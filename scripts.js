932bb999cfac0ca8b450e744ca189e32979b7cf3
dang233 <92148329+dang233@users.noreply.github.com>
2021-11-19 13:41:31 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,35 @@
 var list = document.getElementById("note-list");
+var menu_option = document.getElementById("menu-option");
+var menu_sort = document.getElementById("menu-sort");
+var background = document.getElementById("blur-bg");
+
+//example of selecting all classes
 var tagList = document.querySelectorAll(".note-tag-list");
 
 function noteAddTest() {
     // use this to test the note adding functionality
     var note = document.createElement("div");
     note.classList.add("note-el");
-    note.innerText = "NOOT"
-    list.appendChild(note);
+    note.innerText = "NOTE";
+    list.insertBefore(note, list.childNodes[0]);
 }
 
-for(element of tagList) {
-    element.addEventListener("wheel", (event) => {
-    event.preventDefault();
-    element.scrollLeft += event.deltaY;
-})
+function blurBackground() {
+    background.style.display = "block";
+    background.style.animationName = "blurTransition";
+}
+
+function exitMenu() {
+    background.style.display = "none";
+    background.style.animationName = "";
+}
+
+function showOption() {
+    menu_option.style.animationName = "menuTransition";
+    blurBackground();
+}
+
+function showSort() {
+    menu_sort.style.animationName = "menuTransition";
+    blurBackground();
 }
